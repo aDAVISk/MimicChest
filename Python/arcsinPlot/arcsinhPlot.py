901d293179dda0e819fdbd39e5e313e43c17d315
aDAVISk <akito.davis@gmail.com>
2019-02-17 17:48:59 +0900

    Update and rename Python/arcsinPlot/arcsinhPlot.py to Python/arcsinhPlot/arcsinhPlot.py

--- Python/arcsinPlot/arcsinhPlot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-def setArcsinh(method, *args, dim=1, **kwargs):
-	if not hasattr(dim,"__itr__"):
-		dim = range(dim)
-	args = list(args)
-	for ii in dim:
-		args[ii] = np.arcsinh(args[ii])
-	return method(*args,**kwargs)
---
+++ Python/arcsinhPlot/arcsinhPlot.py
@@ -0,0 +1,19 @@
+##########################################################
+# arcsinhPlot.py
+# Author: Akito D. Kawamura
+# calculate positions of 2D/3D stream line to be plotted.  
+#
+# This program is published under BSD 3-Clause License.
+# This program is a beta version.
+# Please use this program under your responsibility.
+##########################################################
+
+import numpy as np 
+
+def setArcsinh(method, *args, dim=1, **kwargs):
+	if not hasattr(dim,"__itr__"):
+		dim = range(dim)
+	args = list(args)
+	for ii in dim:
+		args[ii] = np.arcsinh(args[ii])
+	return method(*args,**kwargs)
