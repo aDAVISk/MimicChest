15dc6eef00f7496ad0af8d65611abc6870dbe279
aDAVISk <akito.davis@gmail.com>
2022-03-31 10:48:59 +0900

    Update otoshidama.rb

--- Ruby/otoshidama.rb
+++ Ruby/otoshidama.rb
@@ -3,116 +3,103 @@
 # This is a console-based game to move a ball (player)
 # to the bottom floor.
 #
-# Tested on Ruby 2.5.1p57 in WSL Ubuntu
+# Tested on Ruby 3.0.2p107
 # This program is published under BSD 3-Clause License
-# Last Update: 2018/12/31
+# Last Update: 2021/11/26
 ########################################################
 
 require 'io/console'
 
-# dimensions of the map
-$w = 30 #weight
-$h = 15 #height
+h = 8 # height
+d = 12 # width
 
-numHoleMin = 2 #minimum number of holes / floor
-numHoleMax = 4 #maximum number of holes / floor
+$tl = { # map tiles : 表示文字は全角
+  0 => "　", # asle
+  -1 => "\e[42m　\e[0m", # wall # 背景を色指定
+  1 => "\e[33m●\e[0m", # player
+} # 色指定する場合は、必ず\e[0mで閉じ、書式をリセットする事
 
-def getStrPos(pos_x,pos_y)
-  return ($w+1)*($h-pos_y-1)+pos_x
-end
+mapDim = [h*2+1,d+2]
+mapVal = Array.new(mapDim[0]){ Array.new(mapDim[1], 0) }
 
-# Font setup
-wall = "#" 
-ball = "o" 
-blank = " " 
-if wall == ball
-  puts "WRONG SETUP"
-  exit 
-end
-
-# Generating Map
-map = (blank*$w+"\n")*$h
-## Setting up surrounding walls
-for ii in 0..$h-1
-  map[getStrPos(0,ii)] = wall
-  map[getStrPos($w-1,ii)] = wall
-end
-map[0..$w-1] = wall*$w
-map[getStrPos(0,0)..getStrPos($w-1,0)] = wall*$w
-## Generating Floors with holes
-for ii in 0..($h/2-2)
-  numHole = rand(numHoleMin..numHoleMax)
-  floorpos = getStrPos(1,$h-2*ii-3)
-  map[floorpos..(floorpos+$w-2)] = wall*($w-1)
-  trypos = rand($w-1)
-  while numHole > 0
-    if map[floorpos+trypos] == wall
-      map[floorpos+trypos] = blank
-      numHole -=1
-      trypos = rand($w-1)
-    else
-      trypos = (trypos + 1)%($w-1)
+# generating map (walls, floors, and holes)
+(0...mapDim[0]).each do |jj|
+  if jj%2 == 0
+    mapVal[jj].map!{|vv| vv=-1} # floor
+    if jj != 0 and jj != mapDim[0]-1
+      mapVal[jj][Random.rand(1...mapDim[1]-1)] = 0 # hole
     end
+  else
+    mapVal[jj][0] = -1
+    mapVal[jj][-1] = -1
   end
 end
 
-# Initial position setup
-pos_x = $w/2
-pos_y = $h-2
-strpos = getStrPos(pos_x,pos_y)
-floorpos = getStrPos(pos_x,pos_y-1)
-map[floorpos] = wall
-screen = map.dup
-screen[strpos] = ball
+#p mapVal
+def showMapTxt(mapVal)
+  #p $tl
+  hh = mapVal.size()
+  mapTxt = Array.new(hh)
+  (0...hh).each do |jj|
+    mapTxt[jj] = mapVal[jj].map{|ww| $tl[ww]}.join()
+  end
+  #puts "\e[H\e[2J" # clear command line
+  puts "\e[H" # moce cursor to top-left
+  puts mapTxt
+  puts ""
+  return mapTxt
+end
 
-# Initial Screen Setup
-print "OTOSHIDAMA GAME\n"
-print "Use arrow keys <-/-> to move your ball,\n"
-print "and reach to the bottom floor.\n"
-print "Press 'q' to quit the game.\n"
-print "\n"*$h
-print "\e[#{$h}A"
-print screen
+#puts mapTxt
+pos = [1,mapDim[1]/2] # position of player, [vertical, horizontal] from top-left
+if mapVal[pos[0]][pos[1]] == 0
+  pos[1] += 1
+end
+mapVal[pos[0]][pos[1]] = 1
 
-# Game Controll
-while key = STDIN.getch
-  case key
-  when "\e"
-    if STDIN.getch == "["
-      screen[strpos] = map[strpos]
-      case STDIN.getch
-      when "C" #Right
-        if pos_x < $w-2
-          pos_x += 1
-        end
-      when "D" #Left
-        if pos_x > 1
-          pos_x -= 1
-        end
-      end
-      strpos = getStrPos(pos_x,pos_y)
-      screen[strpos] = ball
-    end
-  when "q"
+#logTxt = ""
+
+puts "\e[H\e[2J" # clear command line
+puts "a: move left,  d: move right,     q: quit"
+puts ""
+(-3..-1).each do |tt|
+  puts "\e[1A Starts in #{-tt}"
+  sleep(1)
+end
+
+puts "\e[H\e[2J" # clear command line
+loop do
+  showMapTxt(mapVal)
+  puts "a: move left,  d: move right,     q: quit"
+  key = STDIN.getch
+  #p key
+  #p logTxt += key +":#{key.class},"
+  newPos = pos[1]
+  if key == "q"
+    #print "\e[1;1H\e[#{mapDim[0]+3}B"
+    break
+  elsif key == "a" # move left
+      newPos -= 1
+  elsif key == "d" # move right
+      newPos +=1
+  end
+  if mapVal[pos[0]][newPos] != -1
+    mapVal[pos[0]][pos[1]] = 0
+    pos[1] = newPos
+    mapVal[pos[0]][pos[1]] = 1
+  end
+  while mapVal[pos[0]+1][pos[1]] == 0 # fall slowly
+    showMapTxt(mapVal)
+    sleep(0.3)
+    mapVal[pos[0]][pos[1]] = 0
+    pos[0] += 1
+    mapVal[pos[0]][pos[1]] = 1
+  end
+  if pos[0] == mapDim[0]-2
+    puts "\e[H\e[2J" # clear command line
+    showMapTxt(mapVal)
+    puts "!!! GOAL !!!"
+    puts ""
     break
   end
-  print "\e[#{$h}A\r"
-  print screen
-  floorpos = getStrPos(pos_x,pos_y-1)
-  while map[floorpos] != wall
-    screen[strpos] = map[strpos]
-    pos_y -= 1
-    strpos = getStrPos(pos_x,pos_y)
-    screen[strpos] = ball
-    print "\e[#{$h}A\r"
-    print screen
-    floorpos = getStrPos(pos_x,pos_y-1)
-    sleep(0.02)
-  end
-  if pos_y == 1
-    puts "CONGRATULATIONS!!"
-    break
-  end
-  #  sleep(0.1)
 end
-#end
